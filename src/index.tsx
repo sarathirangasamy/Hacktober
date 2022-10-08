081919bc900d661861f9b511347a1ca9665d9522
Nandhini <42376766+Nandhini1405@users.noreply.github.com>
2022-10-08 17:39:41 +0530

    Coming soon page added

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import CommingSoon from "./coming-soon";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
