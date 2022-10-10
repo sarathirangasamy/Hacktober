a2b8d7505e2281b7ecc88032a7e1a65bb5465fa6
Lalithsaran99 <56213971+Lalithsaran99@users.noreply.github.com>
2022-10-10 11:25:05 +0530

    text changes

--- src/icon.tsx
+++ src/icon.tsx
@@ -6,7 +6,7 @@ export const Icon: React.FC<PropsType> = ({count})=>{
     return (
         <li className="ui dropdown">
       <div className="option_links" title="Cart">
-        <i className="uil uil-shopping-cart-alt"></i>
+        <i className="uil uil-shopping-cart-alt oyo vil kavin"></i>
         {count}
       </div>
     </li>
